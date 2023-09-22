ca1ce6a6261357b3fc17febb314cc0a13a6c223d
Praneeth-Avasarala <praneethavasarala@gmail.com>
2023-09-22 17:10:46 +0530

    updates

--- myform-processor.php
+++ myform-processor.php
@@ -1,1 +1,11 @@
-empty
+<?php
+
+if($_POST["message"]) {
+
+mail("praneethavasarala@gmail.com", "Message on WebPage",
+
+$_POST["Here's the message left on the Webpage"]. "From: praneethavasarala@gmail.com");
+
+}
+
+?>
